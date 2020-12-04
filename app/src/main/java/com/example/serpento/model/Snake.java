a1f303dbed33b7592754664e21715350d1bcd510
Noel <noele@uma.es>
2020-12-04 19:43:59 +0100

    comprobacion german 2

--- app/src/main/java/com/example/serpento/model/Snake.java
+++ app/src/main/java/com/example/serpento/model/Snake.java
@@ -4,12 +4,7 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class Snake {
-
-<<<<<<< Updated upstream
-=======
-    private String hola = "hola german";
-
->>>>>>> Stashed changes
+    
     public static String ARRIBA = "ARRIBA";
     public static String IZQUIERDA = "IZQUIERDA";
     public static String DERECHA = "DERECHA";
